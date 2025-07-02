c7e8ce9bad7f04606803a5c2432a61d09ad660f0
ycamancio <yuricamancio@gmail.com>
2025-07-02 16:27:09 +0100

    Improving error handling inside the API

--- src/main/java/com/sandbox/device/api/controller/ControllerExceptionHandler.java
+++ src/main/java/com/sandbox/device/api/controller/ControllerExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.sandbox.device.api.controller;
 
+import com.sandbox.device.api.controller.response.ErrorApiResponse;
+import com.sandbox.device.api.enums.ErrorType;
 import com.sandbox.device.api.exception.DeviceBusinessRuleException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -12,6 +19,17 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(DeviceBusinessRuleException.class)
     public ResponseEntity<?> handleDeviceBusinessRuleException(DeviceBusinessRuleException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        ErrorApiResponse errorApiResponse = new ErrorApiResponse(ErrorType.BUSINESS_RULE, e.getMessage());
+        return new ResponseEntity<>(errorApiResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        List<ErrorApiResponse> errorList = new ArrayList<>();
+        for(FieldError error : e.getFieldErrors()){
+            errorList.add(new ErrorApiResponse(ErrorType.INVALID_REQUEST, error.getDefaultMessage()));
+        }
+        return new ResponseEntity<>(errorList, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/com/sandbox/device/api/enums/ErrorType.java
@@ -0,0 +1,5 @@
+package com.sandbox.device.api.enums;
+
+public enum ErrorType {
+    BUSINESS_RULE, INVALID_REQUEST;
+}
